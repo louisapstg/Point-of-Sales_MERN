ecdbac4a0efa6de01425f7025baaaea26385828a
Louis <louisapstg@gmail.com>
2023-12-24 21:50:05 +0700

    use cors and cookieParser

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,20 @@
+import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
 import userRoutes from './routes/userRoute.js';
 import errorHandler from './middleware/errorMiddleware.js';
-// import cors from 'cors';
+
 dotenv.config();
 
 const app = express();
 
 // Middleware
+app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
