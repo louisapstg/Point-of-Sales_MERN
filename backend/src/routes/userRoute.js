5083363a4e36b8f551c86c4b4127461ca11920fd
Louis <louisapstg@gmail.com>
2023-12-24 21:49:51 +0700

    add login and logout route

--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -1,7 +1,9 @@
-import { registerUser } from '../controllers/userController.js';
+import { loginUser, logoutUser, registerUser } from '../controllers/userController.js';
 import express from 'express';
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/logout", logoutUser);
 
 export default router;
