6cfa59a346a7d387975717e14b69a67514dc7ae7
Louis <louisapstg@gmail.com>
2023-12-24 21:49:40 +0700

    add login and logout controller

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,6 +1,13 @@
 import asyncHanlder from 'express-async-handler';
 import User from '../models/userModel.js';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
 
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "12h" });
+};
+
+// @desc    Register new user
 const registerUser = asyncHanlder(async (req, res) => {
     const { name, email, password } = req.body;
 
@@ -28,6 +35,18 @@ const registerUser = asyncHanlder(async (req, res) => {
         password,
     });
 
+    // Generate token
+    const token = generateToken(user._id);
+
+    // Send HTTP-only cookie
+    res.cookie("token", token, {
+        path: "/",
+        httpOnly: true,
+        expires: new Date(Date.now() + 43200000),
+        sameSite: "none",
+        secure: true
+    })
+
     if (user) {
         const { _id, name, email, photo, phone, bio } = user;
         res.status(201).json({
@@ -36,7 +55,8 @@ const registerUser = asyncHanlder(async (req, res) => {
             email,
             photo,
             phone,
-            bio
+            bio,
+            token
         });
     } else {
         res.status(400);
@@ -45,6 +65,71 @@ const registerUser = asyncHanlder(async (req, res) => {
 
 });
 
+// @desc    Login user
+const loginUser = asyncHanlder(async (req, res) => {
+    const { email, password } = req.body;
+
+    // Validation
+    if (!email || !password) {
+        res.status(400);
+        throw new Error("Please fill all required fields");
+    }
+
+    // Check if user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+        res.status(401);
+        throw new Error("User not found");
+    }
+
+    // Check if password is correct
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    // Generate token
+    const token = generateToken(user._id);
+
+    // Send HTTP-only cookie
+    res.cookie("token", token, {
+        path: "/",
+        httpOnly: true,
+        expires: new Date(Date.now() + 43200000),
+        sameSite: "none",
+        secure: true
+    })
+
+    if (user && isPasswordCorrect) {
+
+        const { _id, name, email, photo, phone, bio } = user;
+        res.status(200).json({
+            _id,
+            name,
+            email,
+            photo,
+            phone,
+            bio,
+            token
+        });
+    } else {
+        res.status(401);
+        throw new Error("Invalid credentials");
+    }
+});
+
+// @desc    Logout user
+const logoutUser = asyncHanlder(async (req, res) => {
+    res.cookie("token", "", {
+        path: "/",
+        httpOnly: true,
+        expires: new Date(Date.now()),
+        sameSite: "none",
+        secure: true
+    })
+    return res.status(200).json({
+        message: "Logout success"
+    })
+});
+
 export {
-    registerUser
+    registerUser,
+    loginUser,
+    logoutUser
 }
